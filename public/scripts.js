542db993fc9d7a8f23a8773e13f81f91da460459
weilyuwang <weilyuwang@gmail.com>
2020-07-14 15:24:29 -0400

    :fire: send namespaces data to client

--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,32 @@
 const socket = io("http://localhost:9000"); // the /  namespace
-const socket1 = io("http://localhost:9000/wiki"); // the /wiki  namespace
-const socket2 = io("http://localhost:9000/mozilla"); // the /mozilla  namespace
-const socket3 = io("http://localhost:9000/linux"); // lthe /linux namespace
 
 socket.on("connect", () => {
   console.log(socket.id);
 });
 
-socket.on("joined", (msg) => {
-  console.log(msg);
+// listen for nsList event, which contains a list of all namespaces
+socket.on("nsList", (nsData) => {
+  console.log("The list of namespaces has arrived!");
+  console.log(nsData);
 });
 
-socket.on("messageFromServer", (dataFromServer) => {
-  console.log(dataFromServer);
-  socket.emit("messageToServer", { data: "Hello from the Client" });
-});
+// socket.on("messageFromServer", (dataFromServer) => {
+//   console.log(dataFromServer);
+//   socket.emit("messageToServer", { data: "Hello from the Client" });
+// });
 
-document.querySelector("#message-form").addEventListener("submit", (event) => {
-  event.preventDefault();
+// document.querySelector("#message-form").addEventListener("submit", (event) => {
+//   event.preventDefault();
 
-  const newMessage = document.querySelector("#user-message").value;
-  socket.emit("newMessageToServer", { text: newMessage });
+//   const newMessage = document.querySelector("#user-message").value;
+//   socket.emit("newMessageToServer", { text: newMessage });
 
-  // clear out user input
-  var userInput = document.querySelector("#user-message");
-  userInput.value = "";
-});
+//   // clear out user input
+//   var userInput = document.querySelector("#user-message");
+//   userInput.value = "";
+// });
+
+// socket.on("messageToClients", (msg) => {
+//   console.log(msg);
+//   document.querySelector("#messages").innerHTML += `<l1>${msg.text}</l1>`;
+// });
--- slack.js
+++ slack.js
@@ -9,40 +9,24 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
+// main namespace
+io.of("/").on("connect", (socket) => {
+  // build an array to send back with the image and endpoint for each namespace
+  let nsData = namespaces.map((ns) => {
+    return {
+      img: ns.img,
+      endpoint: ns.endpoint,
+    };
+  });
+
+  // send nsData back to client. We need to use socket not io
+  // because we want it to go to just this client
+  socket.emit("nsList", nsData);
+});
+
 // loop through each namespace and listen for a connection
 namespaces.forEach((namespace) => {
   io.of(namespace.endpoint).on("connect", (socket) => {
     console.log(`${socket.id} has joined ${namespace.endpoint}`);
   });
-});
-
-// main namespace
-io.of("/").on("connect", (socket) => {
-  // Send an event from the server to this socket only:
-  socket.emit("messageFromServer", { data: "Welcome to the socket.io server" });
-  socket.on("messageToServer", (dataFromClient) => {
-    console.log(dataFromClient);
-  });
-
-  socket.join("level1");
-
-  /*
-    socket.to => send to everybody in the room except self
-    io.to => sent by server, so everybody in the room including self can see the message
-  */
-
-  socket
-    .to("level1")
-    .emit("joined", `${socket.id} says I have joined the level 1 room`);
-  // io.of("/")
-  //   .to("level1")
-  //   .emit("joined", `${socket.id} says I have joined the level 1 room`);
-});
-
-// /admin namespace
-io.of("/admin").on("connect", (socket) => {
-  console.log("Someone connected to the admin namespace");
-
-  //send a message to the entire namespace
-  io.of("/admin").emit("welcome", "Welcome to the admin channel!");
 });
