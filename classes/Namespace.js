fd8cfa681af96ba3d466c31b8e3c38858029ac4f
weilyuwang <weilyuwang@gmail.com>
2020-07-14 15:02:42 -0400

    :pencil: add classes

--- classes/Namespace.js
+++ classes/Namespace.js
@@ -1,1 +1,14 @@
+class Namespace {
+  constructor(id, nsTitle, img, endpoint) {
+    this.id = id;
+    this.nsTitle = nsTitle;
+    this.img = img;
+    this.endpoint = endpoint;
+  }
 
+  addRoom(roomObj) {
+    this.rooms.push(roomObj);
+  }
+}
+
+module.exports = Namespace;
