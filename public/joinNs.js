7f02f8fb1377dfb270b21db4967a6f1469eb1535
weilyuwang <weilyuwang@gmail.com>
2020-07-14 16:17:28 -0400

    :art: refactor

---
+++ public/joinNs.js
@@ -0,0 +1,41 @@
+function joinNs(endpoint) {
+  const nsSocket = io(`http://localhost:9000${endpoint}`);
+
+  nsSocket.on("nsRoomLoad", (nsRooms) => {
+    let roomList = document.querySelector(".room-list");
+    roomList.innerHTML = "";
+    nsRooms.forEach((room) => {
+      let glyph;
+      if (room.privateRoom) {
+        glyph = "lock";
+      } else {
+        glyph = "globe";
+      }
+      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
+    });
+    // Add click listener to each room
+    const roomNodes = document.getElementsByClassName("room");
+    Array.from(roomNodes).forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        console.log("Someone clicked on ", e.target.innerText);
+      });
+    });
+  });
+
+  nsSocket.on("messageToClients", (msg) => {
+    console.log(msg);
+    document.querySelector("#messages").innerHTML += `<li>${msg.text}</li>`;
+  });
+
+  document
+    .querySelector(".message-form")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const newMessage = document.querySelector("#user-message").value;
+      socket.emit("newMessageToServer", { text: newMessage });
+
+      // clear out user input
+      var userInput = document.querySelector("#user-message");
+      userInput.value = "";
+    });
+}
--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,5 @@
 const socket = io("http://localhost:9000"); // the /  namespace
 
-socket.on("connect", () => {
-  console.log(socket.id);
-});
-
 // listen for nsList event, which contains a list of all namespaces
 socket.on("nsList", (nsData) => {
   console.log("The list of namespaces has arrived! Check below");
@@ -25,26 +21,5 @@ socket.on("nsList", (nsData) => {
     });
   });
 
-  const nsSocket = io("http://localhost:9000/wiki");
-  nsSocket.on("nsRoomLoad", (nsRooms) => {
-    let roomList = document.querySelector(".room-list");
-    roomList.innerHTML = "";
-    nsRooms.forEach((room) => {
-      let glyph;
-      if (room.privateRoom) {
-        glyph = "lock";
-      } else {
-        glyph = "globe";
-      }
-      roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`;
-    });
-    // Add click listener to each room
-    const roomNodes = document.getElementsByClassName("room");
-    Array.from(roomNodes).forEach((elem) => {
-      elem.addEventListener("click", (e) => {
-        console.log("Someone clicked on ", e.target.innerText);
-      });
-    });
-    console.log(roomNodes);
-  });
+  joinNs("/wiki");
 });
