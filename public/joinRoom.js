75188fd0c2671a3201b1f4143e43eb369f825df7
weilyuwang <weilyuwang@gmail.com>
2020-07-14 23:33:10 -0400

    :sparkles: search box

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -26,7 +26,22 @@ function joinRoom(roomName) {
       ".curr-room-num-users"
     ).innerHTML = `${num} <span class="glyphicon glyphicon-user"></span
     >`;
-
     document.querySelector(".curr-room-text").innerText = `${roomName}`;
+  });
+
+  let searchBox = document.querySelector("#search-box");
+  searchBox.addEventListener("input", (e) => {
+    let messages = Array.from(document.getElementsByClassName("message-text"));
+
+    messages.forEach((msg) => {
+      if (
+        msg.innerText.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1
+      ) {
+        // The msg does not contain the user search term!
+        msg.style.display = "none";
+      } else {
+        msg.style.display = "block";
+      }
+    });
   });
 }
